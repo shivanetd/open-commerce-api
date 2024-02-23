8341df5d987bb938336eceed9d27de977b31530b
shiva n <shiva.netd@gmail.com>
2024-02-23 16:29:41 -0500

    init commit

---
+++ src/main/java/com/shiva/opencommerceapi/OpenCommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.shiva.opencommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenCommerceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiva/opencommerceapi/OpenCommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiva.opencommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenCommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
